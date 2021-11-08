2499e496400a0851cbb6e28455b6260ab5e586e9
volta <57532820+vomar18@users.noreply.github.com>
2021-11-08 17:44:24 +0100

    Update 00_check_a_DATASET_PANDAS.py

--- tips/00_check_a_DATASET_PANDAS.py
+++ tips/00_check_a_DATASET_PANDAS.py
@@ -3,3 +3,6 @@ import pandas as pd
 melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path) 
 melbourne_data.columns
+
+# dropna drops missing values (think of na as "not available")
+melbourne_data = melbourne_data.dropna(axis=0)
