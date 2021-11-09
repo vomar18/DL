7f52b177d550b2ef5e2aaa628ec2bcfc1f49d6b8
volta <57532820+vomar18@users.noreply.github.com>
2021-11-09 14:00:43 +0100

    Create 000_create_a_matrix_TORCH.py

---
+++ tips/000_create_a_matrix_TORCH.py
@@ -0,0 +1,8 @@
+# import pytorch library
+import torch
+
+# pytorch array
+tensor = torch.Tensor(array)
+print("Array Type: {}".format(tensor.type)) # type
+print("Array Shape: {}".format(tensor.shape)) # shape
+print(tensor)
