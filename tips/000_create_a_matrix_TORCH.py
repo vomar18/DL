528d3ef80d2ff51a2bbec435d0201f0524dc5e0c
volta <57532820+vomar18@users.noreply.github.com>
2021-11-09 14:58:09 +0100

    Update 000_create_a_matrix_TORCH.py

--- tips/000_create_a_matrix_TORCH.py
+++ tips/000_create_a_matrix_TORCH.py
@@ -2,7 +2,28 @@
 import torch
 
 # pytorch array
-tensor = torch.Tensor(array)
+tensor = torch.Tensor(array) # array è del tipo [a, x, c, f] quindi devi sempre usare un vettore come contenitore
 print("Array Type: {}".format(tensor.type)) # type
 print("Array Shape: {}".format(tensor.shape)) # shape
 print(tensor)
+
+# sovrascrivere dati pseudocasuali in torch
+x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
+print(f"Random Tensor: \n {x_rand} \n")
+shape = (2, 3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+
+#CONCATENATE A SEQ OF TENSORRS
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
+
+# This computes the element-wise product
+print(f"tensor.mul(tensor) \n {tensor.mul(tensor)} \n")
+# Alternative syntax:
+print(f"tensor * tensor \n {tensor * tensor}")
